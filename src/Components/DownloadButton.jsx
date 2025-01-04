519fd57afdb0e38de4c183560f1f402e1f9c90c9
@maptaul <maptaulislam1@gmail.com>
2025-01-05 01:33:47 +0600

    readme

--- src/Components/DownloadButton.jsx
+++ src/Components/DownloadButton.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import maptaulPdf from "../../public/Maptaul.pdf";
 
 const DownloadButton = () => {
   return (
     <div className="text-center mt-6">
       <a
-        href="/public/Maptaul.pdf" // Ensure the file is in the 'public' directory of your React app
+        href={maptaulPdf} // Ensure the file is in the 'public' directory of your React app
         download="Maptaul.pdf" // Sets the name for the downloaded file
         className="btn btn-primary px-6 py-3 rounded-md bg-orange-500 hover:bg-orange-600 text-white transition duration-300"
         aria-label="Download CV"
